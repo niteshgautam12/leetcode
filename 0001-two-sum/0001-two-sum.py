6dca4fe63b851902c6c4db9661a165faa5fe831c
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2025-03-01 14:34:52 +0530

    Time: 3 ms (61%), Space: 13.1 MB (62.46%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hash_map = {}
+        for i , num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return[hash_map[complement],i]
+            hash_map[num] = i
+        return []
+        
