971dd7a29ef2ef82aa91696a6ea81e11fc05021d
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2025-05-11 22:33:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (46.71%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        if len(arr) <= 2 :
+            return False
+        for i in range (len(arr)-2):
+            if arr[i]%2 != 0 and arr[i+1]%2 != 0 and arr[i+2]%2 != 0:
+                return True
+
+        return False
+
+        
