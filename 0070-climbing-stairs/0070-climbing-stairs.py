e99a6cb40d0e6ae06c721a8656f0336dd783dea8
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2025-05-16 14:41:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n >= 4 :
+            return n+1
+        return n
+        
