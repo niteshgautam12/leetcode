a2e7998b836cc52504fee4b4c0a0542fbf3f2819
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2025-05-16 14:53:54 +0530

    Time: 0 ms (100%), Space: 12.3 MB (81.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,7 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n >= 4 :
-            return n+1
-        return n
+        if n == 1 :
+            return 1
+        if n == 2 :
+            return 2
+        count1 = 1
+        count2 = 2
+        for i in range (2,n):
+            count1,count2 = count2,count2 +count1
+        return count2
+                
         
