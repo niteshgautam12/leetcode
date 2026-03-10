1012887b4cb2df94a54ed6c4f3d8cdebc2dc026e
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2026-03-10 11:25:23 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,34 +1,23 @@
-
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-       
-        dummy = ListNode()
-        current = dummy
-        
-       
-        while list1 and list2:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        d = ListNode
+        curr = d
+        while  list1 and list2 :
             if list1.val < list2.val:
-                current.next = list1
+                curr.next = list1
+                curr = list1
                 list1 = list1.next
-            else:
-                current.next = list2
+            else :
+                curr.next = list2
+                curr = list2
                 list2 = list2.next
-            current = current.next
+        curr.next = list1 if list1 else list2
+        return d.next
+                
 
         
-        if list1:
-            current.next = list1
-        if list2:
-            current.next = list2
-
-        return dummy.next  
