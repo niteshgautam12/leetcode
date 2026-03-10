6adf0b504ecdf414d919af45a47b76f94b80c282
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2026-03-10 12:50:09 +0530

    Time: 1966 ms (7.24%), Space: 19.9 MB (80.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}  
-
-        for i, n in enumerate(nums):
-            complement = target - n
-
-            if complement in d:
-                return [d[complement], i]
-
-            d[n] = i
+        new = [] 
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target :
+                    new.append(i)
+                    new.append(j)
+        return new
