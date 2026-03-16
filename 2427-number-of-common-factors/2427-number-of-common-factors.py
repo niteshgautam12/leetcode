6659199f091dabcb3bce4fe253a4daa0ffaef3e9
Nitesh Gautam <138124708+niteshgautam12@users.noreply.github.com>
2026-03-16 13:41:22 +0530

    Time: 2 ms (29.69%), Space: 12.4 MB (49.04%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -6,23 +6,7 @@ class Solution(object):
         :rtype: int
         """
         count = 0 
-
-        p = []
-        l =[]
-        for i in range (1,a):
-            if a%i == 0 :
-                p.append(i)
-        for i in range(1,b):
-            if b%i == 0:
-                l.append(i)
-        c = p + l
-        c.sort()
-        for i in range (len (c)-1):
-            if c[i] == c[i+1]:
+        for i in range (1,min(a,b)+1):
+            if a%i == 0 and b % i == 0 :
                 count += 1
-        if max(a,b)%min(a,b) == 0:
-            count += 1
         return count
-
-
-        
